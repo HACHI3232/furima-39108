ef340136a41aea067cdca12ca87cfd48e498ee1b
HACHI3232 <harada.chisato526@gmail.com>
2023-03-28 18:25:46 +0900

    販売価格の変動の追加

---
+++ app/javascript/packs/fee_calculation.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", () => {
+  const priceInput = document.getElementById("item-price");
+  const addTaxDom = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+  console.log(priceInput);
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+    profit.innerHTML = inputValue - Math.floor(inputValue * 0.1);
+  });
+});
