c372997517ba4eba6f70e4789714fc1996a904ae
HACHI3232 <harada.chisato526@gmail.com>
2023-03-29 18:51:36 +0900

    FactryBotの設定の記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    association :user, factory: :user
+    name { Faker::Commerce.product_name }
+    description { Faker::Lorem.sentence }
+    category { Category.all.sample }
+    condition { Condition.all.sample }
+    shipping_payer { ShippingPayer.all.sample  }
+    prefecture { Prefecture.all.sample }
+    shipping_day { ShippingDay.all.sample }
+    price { Faker::Commerce.price(range: 300..9_999_999) }
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
+
+
