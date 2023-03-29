91e0e0bb3ef3adae3f9a6d7fe76a4033c6c6b5de
HACHI3232 <harada.chisato526@gmail.com>
2023-03-30 00:46:11 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,19 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
-  # テーブルのバリデーション(空だと登録できない)
+  #カラムのバリデーション(空だと登録できない)
   with_options presence: true do
-    validates :image, :name, :description, :category_id, :condition_id, :shipping_payer_id, :prefecture_id, :shipping_day_id,
-              exclusion: { in: ['---'] }
+    validates :image, :name, :description, :category_id, :condition_id, :shipping_payer_id, :prefecture_id, :shipping_day_id
   end
+    # 価格のバリデーション（300円以上、9.999.999円以下）でないと登録出来ない
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
-  # 価格のバリデーション
-  validates :price, presence: true,
-                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+
+
+  #プルダウンのバリデーション未選択（id=0）の場合登録出来ない
+with_options numericality: { other_than: 0 , message: "can't be blank"} do
+  validates :category_id, :condition_id, :shipping_payer_id, :prefecture_id, :shipping_day_id
+end
 
   # アソシエーション
   belongs_to :category
@@ -21,3 +25,14 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 end
+
+
+# validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+# validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+# validates :shipping_payer_id, numericality: { other_than: 1 , message: "can't be blank"}
+# validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+# validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+
+# with_options numericality: { other_than: 1 , message: "can't be blank"} do
+#   validates :category_id, :condition_id, :shipping_payer_id, :prefecture_id, :shipping_day_id
