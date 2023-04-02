eae76c09bbdb6b1d3d1656b5fde7ae99f94294f7
HACHI3232 <harada.chisato526@gmail.com>
2023-04-02 22:40:22 +0900

    oederAddressのバリデーション追加

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,3 +1,10 @@
 class OrderAddress < ApplicationRecord
   belongs_to :order
+
+  with_options presence: true do
+    validates :postcode, :prefecture_id, :city, :block
+    validates :postcode, format: { with: /\A\d{3}-\d{4}\z/ }
+    validates :prefecture_id, numericality: { other_than: 0,}
+    validates :phone_number, format: { with: /\A\d{10,11}\z/,}
+  end
 end
