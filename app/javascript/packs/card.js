60c44afad4a58221aae66780fc180220ea6783e4
HACHI3232 <harada.chisato526@gmail.com>
2023-04-03 16:19:03 +0900

    pay.jpのjs記述①

---
+++ app/javascript/packs/card.js
@@ -0,0 +1,26 @@
+const pay = () => {
+  const payjp = Payjp("pk_test_c11eb9ec0c9312cd5518c01f"); // PAY.JPテスト公開鍵
+  const form = document.getElementById("charge-form");
+  const elements = payjp.elements();
+  const numberElement = elements.create("cardNumber");
+  const expiryElement = elements.create("cardExpiry");
+  const cvcElement = elements.create("cardCvc");
+
+  numberElement.mount("#number-form");
+  expiryElement.mount("#expiry-form");
+  cvcElement.mount("#cvc-form");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    payjp.createToken(numberElement).then(function (response) {
+      if (response.error) {
+      } else {
+        const token = response.id;
+        console.log(token);
+      }
+    });
+  });
+};
+
+window.addEventListener("load", pay);
