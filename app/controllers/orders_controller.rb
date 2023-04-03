41ebc524d60ccdf7160527bacc6ab3b10f0f6065
HACHI3232 <harada.chisato526@gmail.com>
2023-04-03 17:15:39 +0900

    pay_itemの定義追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,10 +3,12 @@ class OrdersController < ApplicationController
 
   def index
     @order_form = OrderForm.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
     @order_form = OrderForm.new(order_params)
+    pay_item
     if @order_form.valid?
       @order_form.save
       redirect_to root_path
@@ -18,6 +20,25 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_form).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id,item_id:params[:item_id] )
+    params.require(:order_form).permit(
+      :postcode,
+      :prefecture_id,
+      :city,
+      :block,
+      :building,
+      :phone_number
+    ).merge(
+      user_id:current_user.id,
+      item_id:params[:item_id],
+    )
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    customer = Payjp::Customer.create(
+      description: 'card',
+      card: params[:token]
+    )
+  end
+
 end
