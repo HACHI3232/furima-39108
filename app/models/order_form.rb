fd9c677d62d0030d02753652db4ad14c05471665
HACHI3232 <harada.chisato526@gmail.com>
2023-04-04 20:00:33 +0900

    バリデーション変更

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -5,7 +5,7 @@
     with_options presence: true do
       validates :postcode, :prefecture_id, :city, :block,:user_id,:item_id,:token
       validates :postcode, format: { with: /\A\d{3}-\d{4}\z/ }
-      validates :prefecture_id, numericality: { other_than: 0,}
+      validates :prefecture_id, numericality: { other_than: 1 }
       validates :phone_number, format: { with: /\A\d{10,11}\z/,}
     end
 
