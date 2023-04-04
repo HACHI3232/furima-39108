467f8efb13922b7181b55672b5967e330a455e4d
HACHI3232 <harada.chisato526@gmail.com>
2023-04-04 18:43:01 +0900

    バリデーション記述移動

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,6 +1,13 @@
   class OrderForm
     include ActiveModel::Model
-    attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :city, :block, :building, :phone_number
+    attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :city, :block, :building, :phone_number,:token
+
+    with_options presence: true do
+      validates :postcode, :prefecture_id, :city, :block,:user_id,:item_id,:token
+      validates :postcode, format: { with: /\A\d{3}-\d{4}\z/ }
+      validates :prefecture_id, numericality: { other_than: 0,}
+      validates :phone_number, format: { with: /\A\d{10,11}\z/,}
+    end
 
     def save
       order = Order.create(user_id: user_id, item_id: item_id)
