ac20c8cf5ccd8b392d44ccfb6c5d84d94df60aeb
HACHI3232 <harada.chisato526@gmail.com>
2023-04-04 20:01:03 +0900

    FactoryBotの内容修正

--- spec/factories/order_forms.rb
+++ spec/factories/order_forms.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     user_id { Faker::Number.non_zero_digit }
     item_id { Faker::Number.non_zero_digit }
     postcode { '123-4567' }
-    prefecture_id { 1 }
+    prefecture_id { 2 }
     city { '東京都' }
     block { '渋谷区道玄坂1-1-1' }
     building { '渋谷ビルディング' }
