f79ca73e4e705d8b37e1f9d2199c5ebf1a10c7e2
HACHI3232 <harada.chisato526@gmail.com>
2023-04-04 22:37:19 +0900

    修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show, :index]
   before_action :set_item, only: [:index, :create]
 
   def index
